5a69478dc810ca77fdae64bdf5601fd1c67fe873
Unwuchola, Eneojo <eneojounwuchola@my.unt.edu>
2025-08-28 23:06:53 -0500

    Mock Data setup

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ import express from 'express'
 import lang from './routes/langRoutes.js'
 import everything from './routes/everything.js'
 import categories from './routes/categoryRoutes.js'
+import fs from "fs/promises"
 const app = express();
 const port = process.env.PORT || 3000
+
+
 
 
 // Mock data
@@ -18,7 +21,6 @@ const newsArticle = {
 }
 
 const newsArticle_v2 = {
-
     title: "Aws adds sdk to Go langauge ",
     summary: "This article talks about aws building support for go ",
     src: "",
@@ -27,7 +29,21 @@ const newsArticle_v2 = {
     Date: ""
 }
 
+let data = JSON.stringify(newsArticle, null, 2)
 
+
+async function jsonWrite() {
+    let fileHandle;
+    try {
+        fileHandle = await fs.open('categories.json', 'a');
+        await fileHandle.write(data, 'utf8', () => console.log("fILE CONTENT WRITTEN TOO"))
+        console.log(data)
+    }
+    catch (err) {
+        console.error('WRITING ERROR', err)
+    }
+
+}
 
 
 
@@ -41,4 +57,6 @@ app.use('/languages', lang)
 app.use('/categories', categories)
 
 
+
+jsonWrite();
 app.listen(port, () => console.log(`Server is running on ${port}`))
