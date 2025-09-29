416e175e9d34215e4a90b9ac18ed46c54e1d13fa
Unwuchola, Eneojo <eneojounwuchola@my.unt.edu>
2025-09-28 23:24:38 -0500

    Cron Schedule function added and in testing

--- server.js
+++ server.js
@@ -5,10 +5,48 @@ import categories from './routes/categoryRoutes.js'
 import fs from "fs/promises"
 import databseConnect from './Middleware/databaseConnect.js'
 import errorHandler from './Middleware/error.js'
+import cron from "node-cron"
+
 
 
 const app = express();
 const port = process.env.PORT || 8000
+
+cron.schedule("*/10 * * * *", async function () {
+    const databaseUpdated = false;
+    const newPost = false;
+    const currentDate = new Date();
+
+    try {
+        console.log("Lol") // would ping streams
+
+    }
+    catch (error) {
+        console.log("Lol") // would ping streams
+    }
+    finally {
+        const transporter = nodemailer.createTransport(
+            {
+                secure: true,
+                host: 'smtp.gmail.com',
+                port: 465,
+                auth: {
+                    user: 'eneojo.solomon.u@gmail.com',
+                    pass: process.env.EMAIL_PASS
+                }
+            }
+        );
+
+        await transporter.verify();
+        let info = await transporter.sendMail({
+            to: "solomonunwuchola@gmail.com",
+            subject: `Cron service ${currentDate}`,
+            text: `DATABASE UPDATED : ${databaseUpdated}`,
+
+        })
+        console.log("Sent", info.messageId)
+    }
+});
 
 
 // Body parser 
@@ -24,6 +62,7 @@ app.use(databseConnect)
 app.use('/news', everything)
 app.use('/languages', lang)
 app.use('/categories', categories)
+
 
 
 app.use((req, res, next) => {
